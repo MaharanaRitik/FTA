7c759172af82e37ae793853c5337b6de5f2716fb
Ritik Dubey <ritik.dubey.321@gmail.com>
2023-05-29 14:48:06 +0530

    Ritik Commit 1.0

--- app/src/main/java/com/example/fta/Workout.java
+++ app/src/main/java/com/example/fta/Workout.java
@@ -23,12 +23,10 @@ public class Workout extends AppCompatActivity {
         arrExercise.add(new exerciseModel(R.raw.jumping_jacks, "Jumping_Jacks", "1 min", "hehe"));
         arrExercise.add(new exerciseModel(R.raw.jumping_squats, "Jumping_squats", "1 min", "hehe"));
         arrExercise.add(new exerciseModel(R.raw.lunge, "lunge 1", "3 min", "hehe"));
-        arrExercise.add(new exerciseModel(R.raw.lunges, "lunge 2", "3 min", "hehe"));
-        arrExercise.add(new exerciseModel(R.raw.push_ups, "push up 1", "1.5 min", "hehe"));
-        arrExercise.add(new exerciseModel(R.raw.pushup, "push up 2", "100 Sec", "hehe"));
-        arrExercise.add(new exerciseModel(R.raw.side_plank, "side plank", "1.5 min", "hehe"));
         arrExercise.add(new exerciseModel(R.raw.squat_kicks, "squat kicks", "1 min", "hehe"));
+        arrExercise.add(new exerciseModel(R.raw.side_plank, "side plank", "1.5 min", "hehe"));
         arrExercise.add(new exerciseModel(R.raw.t_plank_exercise, "plank 2", "?", "hehe"));
+        arrExercise.add(new exerciseModel(R.raw.push_ups, "push up 1", "1.5 min", "hehe"));
 
         recyclerWorkoutAdapter adapter = new recyclerWorkoutAdapter(this, arrExercise);
         exRecyclerViewList.setAdapter(adapter);
---
+++ app/src/main/java/com/example/fta/deepSleep.java
@@ -0,0 +1,16 @@
+package com.example.fta.data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fta.R;
+
+public class deepSleep extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deep_sleep);
+    }
+}
